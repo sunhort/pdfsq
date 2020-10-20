a6c2bb82108f4a31d0f5256373e677bcc6f560ba
sunhort <47280098+sunhort@users.noreply.github.com>
2020-10-20 21:36:43 +0300

    Update pdfsq.sh

--- pdfsq.sh
+++ pdfsq.sh
@@ -7,12 +7,9 @@ echo '
            █▄ ▄█ █  █ █    █          █     ▀                     ▀███▀    █    ▐  █      ▀      
             ▀▀▀  █   ██   ▀          ▀                                    ▀         ▀            
 '
-# before running install software with command sudo apt install webp parallel -y
-# uncomment strings below as needed
-
 echo 'Encoding files, do not cancel or interrupt in order to avoid the loss of data...'
 sleep 2
-# compress pdfs
+
 echo 'Rename to match *.pdf names'
 sleep 2
 find . -name "*.PDF"  -exec sh -c 'mv "$1" "${1%.PDF}.pdf"' _ {} \;
@@ -25,6 +22,7 @@ mv ./pdfcache.lst ./pdfcache_done.lst;
 find . -name "*.pdf" | parallel --bar --eta 'echo {}>>pdfcache.lst';
 awk 'NR == FNR { list[tolower($0)]=1; next } { if (! list[tolower($0)]) print }' pdfcache_done.lst pdfcache.lst>>pdfcache_new.lst;
 rm -f pdfcache.lst;
+
 echo 'Compress with ghostscript';
 sleep 2;
 cat pdfcache_new.lst | parallel --bar --eta '/home/ubuntu/ghostscript-9.53.2-linux-x86_64/gs-9532-linux-x86_64 -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dConvertCMYKImagesToRGB=true -dDetectDuplicateImages=true -dPrinted=false -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile={.}_compressed.pdf {} && mv {.}_compressed.pdf {}';
